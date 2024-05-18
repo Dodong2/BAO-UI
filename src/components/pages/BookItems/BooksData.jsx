618d52a6d69160340b2dab7e43abfdbda99cdb75
Dodong2 <carlseighartaliodegmail.com>
2024-05-18 10:48:11 +0800

    updated UI may 18

--- src/components/pages/BookItems/BooksData.jsx
+++ src/components/pages/BookItems/BooksData.jsx
@@ -2,77 +2,77 @@
 export const BooksData = [
 
     {
-        img: "/src/components/image/b1.jpg",
+        img: "/src/components/image/b1.png",
         productName: "Teaching and Assessment of the MACROSKILLS",
         price: "P 420"
     },
     {
-        img: "/src/components/image/b2.jpg",
+        img: "/src/components/image/b2.png",
         productName: "Field Study 2",
         price: "P 420"
     },
     {
-        img: "/src/components/image/b3.jpg",
+        img: "/src/components/image/b3.png",
         productName: "Children and Adolescent Literature",
         price: "P 420"
     },
     {
-        img: "/src/components/image/b4.jpg",
+        img: "/src/components/image/b4.png",
         productName: "Field Study 1",
         price: "P 420"
     },
     {
-        img: "/src/components/image/b5.jpg",
+        img: "/src/components/image/b5.png",
         productName: "Trends and Issues in Social Studies",
         price: "P 420"
     },
     {
-        img: "/src/components/image/b6.jpg",
+        img: "/src/components/image/b6.png",
         productName: "The teaching and Assessment of Literature Studies",
         price: "P 420"
     },
     {
-        img: "/src/components/image/b7.jpg",
+        img: "/src/components/image/b7.png",
         productName: "Speech and Theater Arts",
         price: "P 420"
     },
     {
-        img: "/src/components/image/b8.jpg",
+        img: "/src/components/image/b8.png",
         productName: "Teaching Multigrade Classes",
         price: "P 420"
     },
     {
-        img: "/src/components/image/b9.jpg",
+        img: "/src/components/image/b9.png",
         productName: "Kitchen Essentials and Basic Food Prapartion",
         price: "P 420"
     },
     {
-        img: "/src/components/image/b10.jpg",
+        img: "/src/components/image/b10.png",
         productName: "Tourism and Hostpitality Market",
         price: "P 420"
     },
     {
-        img: "/src/components/image/b11.jpg",
+        img: "/src/components/image/b11.png",
         productName: "Macro Perspective of Tourism and Hostpitality",
         price: "P 420"
     },
     {
-        img: "/src/components/image/b12.jpg",
+        img: "/src/components/image/b12.png",
         productName: "Communication for Society",
         price: "P 420"
     },
     {
-        img: "/src/components/image/b13.jpg",
+        img: "/src/components/image/b13.png",
         productName: "Tour and Travel Management",
         price: "P 420"
     },
     {
-        img: "/src/components/image/b14.jpg",
+        img: "/src/components/image/b14.png",
         productName: "Understanding Self",
         price: "P 300"
     },
     {
-        img: "/src/components/image/b15.jpg",
+        img: "/src/components/image/b15.png",
         productName: "Risk Management as Applied to Safety, Security, and Sanitation",
         price: "P 420"
     },
